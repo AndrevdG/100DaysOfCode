f618f603c2b96f1d0b3c63564daa9991cc4f3c26
Andre van der Goes <andreg@macaw.nl>
2025-02-25 20:11:46 +0100

    adding try/except for day_29

--- day_26_NATO_alphabet_encoder/main.py
+++ day_26_NATO_alphabet_encoder/main.py
@@ -7,7 +7,14 @@ nato_abc = {row.letter: row.code for (i, row) in nato_abc_df.iterrows()}
 
 # TODO 2: Create a list of the phonetic code words
 # from a word that the user inputs
-word_to_spell = input("Which word would you like to be spelled phonetically? : ")
-word_phony = [nato_abc[letter.upper()] for letter in word_to_spell]
 
-print(word_phony)
+# try: from day_30
+while True:
+    word_to_spell = input("Which word would you like to be spelled phonetically? : ")
+    try:
+        word_phonetic = [nato_abc[letter.upper()] for letter in word_to_spell]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
+    else:
+        print(word_phonetic)
+        break
